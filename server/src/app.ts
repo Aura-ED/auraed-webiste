f1ea618288e873a4f10a6c8fad6763182855d3ab
Sovit Lekhak <scifi@macbook-pro.mynetworksettings.com>
2022-07-28 11:33:53 -0400

    feat: Added refactored error class

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 const blogRouter = require('./routes/blogRoutes');
 const eventRouter = require('./routes/eventRoutes');
+import { Request, Response, NextFunction } from 'express';
+import { AppError, handleError } from './utils/appError';
 
 const app = express();
 
@@ -13,5 +15,17 @@ if (process.env.NODE_ENV == 'development') {
 
 app.use('/api/blogs', blogRouter);
 app.use('/api/events', eventRouter);
+
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+  handleError(err, res);
+});
+
+app.use('*', (req: Request, res: Response, next) => {
+  next(new AppError(404, '404 Not Found'));
+});
+
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+  handleError(err, res);
+});
 
 module.exports = app;
