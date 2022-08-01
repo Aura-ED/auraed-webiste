6f709aeaa08a2b8e64f1389b15496ac780eb00a4
sovitpy <sovitlekhak@gmail.com>
2022-08-01 10:51:56 -0400

    feat: Added some mongo error handlers.

--- server/src/utils/errorUtils.ts
+++ server/src/utils/errorUtils.ts
@@ -8,19 +8,28 @@ export class AppError extends Error {
   }
 }
 
+const handleCastErrorDB = (err: AppError) => {
+  const message = `Invalid value "${err.value}" for the field ${err.path}.`;
+  const error = new AppError(400, message);
+  return error;
+};
+
+const handleDuplicateFieldsDB = (err: AppError) => {
+  const value = err.message
+    .match(/(?<=\{)(.*?)(?=\})/)[0]
+    .split(': ')
+    .map((x) => x.trim());
+  const message = `Duplicate field value: ${value[1]} for field: ${value[0]}. Please use another value!`;
+  return new AppError(400, message);
+};
+
 const sendErrorProd = (err: AppError, res: Response) => {
   if (err.sendProd) {
     res.status(err.statusCode).json({
       status: 'fail',
       message: err.message,
     });
-  } else if (err.name == 'CastError') {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
   } else {
-    console.log(err.name);
     res.status(500).json({
       status: 'fail',
       message: 'Something went wrong',
@@ -29,25 +38,28 @@ const sendErrorProd = (err: AppError, res: Response) => {
 };
 
 const sendErrorDev = (err: AppError, res: Response) => {
-  res.status(err.statusCode).json({
+  res.status(err.statusCode || 500).json({
     status: 'fail',
     message: err.message,
     stack: err.stack,
   });
 };
 
-export const handleError = (err: AppError, res: Response) => {
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
-  }
-
-  if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
-  }
-};
-
 export const catchAsync = (fn: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
     fn(req, res, next).catch(next);
   };
+};
+
+export const handleError = (err: AppError, res: Response) => {
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+  if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+    if (err.name == 'CastError') error = handleCastErrorDB(error);
+    else if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    sendErrorProd(error, res);
+  }
 };
