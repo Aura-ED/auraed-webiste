014f6cf0146227b54fa6d643868792005c3ea4a7
Sovit Lekhak <scifi@macbook-pro.mynetworksettings.com>
2022-08-01 10:39:05 -0400

    feat: Updated blog and event models

--- server/src/models/eventModel.ts
+++ server/src/models/eventModel.ts
@@ -4,20 +4,60 @@ const eventSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: true
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
     },
     images: [
       {
         type: String,
-        required: true
-      }
+        required: false,
+      },
     ],
     description: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+    },
+    membersInvolved: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
   },
   { timestamps: true }
 );
+
+eventSchema.virtual('eventStatus').get(function () {
+  const now = new Date();
+  const startDate = this.startDate;
+  const endDate = this.endDate;
+  if (endDate) {
+    if (now < startDate) {
+      return 'Upcoming';
+    } else if (now > startDate && now < endDate) {
+      return 'Ongoing';
+    } else if (now > endDate) {
+      return 'Ended';
+    }
+  } else {
+    if (now < startDate) {
+      return 'Upcoming';
+    } else if (now > startDate) {
+      return 'Ongoing';
+    } else if (now > startDate) {
+      return 'Ended';
+    }
+  }
+});
 
 export const Event = mongoose.model('Event', eventSchema);
