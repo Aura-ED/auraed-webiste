35d3e369f064d7a496e02e485e5d681b71f05178
lamdiv <imdiwash.mrprogrammer@gmail.com>
2022-08-31 21:23:37 +0545

    🚑🔮 UI fixes

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,29 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'opensans' : ['Open Sans', 'sans-serif'],
-        'quicksand': ['Quicksand', 'sans-serif'], 
+        opensans: ["Open Sans", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
       },
-      screens:{
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px'  
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1200px",
+        // xxl: "1440px",
       },
-      
-        colors:{
-          primary: '#4FA3A5',
-          primaryDark: '#4D727A',
-          primaryLight: '#F4FFFF',
-          dark: '#403939',
-        }
-      
+
+      colors: {
+        primary: "#4FA3A5",
+        primaryDark: "#4D727A",
+        primaryLight: "#F4FFFF",
+        dark: "#403939",
+      },
     },
   },
   plugins: [],
-}
+};
