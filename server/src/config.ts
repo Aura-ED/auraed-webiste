6551c1aa6c82c6992ebb3ac57cbd39c06dfd80aa
sovitpy <sovitlekhak@gmail.com>
2022-09-23 20:49:27 -0500

    fix: port and roman pic

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,8 @@
-export const NODE_ENV = process.env.NODE_ENV || "development";
+export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const JWT_ACCESS_SECRET =
-  process.env.JWT_ACCESS_SECRET || "accessSecret";
+  process.env.JWT_ACCESS_SECRET || 'accessSecret';
 export const JWT_REFRESH_SECRET =
-  process.env.JWT_REFRESH_SECRET || "refreshSecret";
-export const JWT_ACCESS_EXPIRES = process.env.JWT_ACCESS_EXPIRES || "1d";
-export const JWT_REFRESH_EXPIRES = process.env.JWT_REFRESH_EXPIRES || "30d";
+  process.env.JWT_REFRESH_SECRET || 'refreshSecret';
+export const JWT_ACCESS_EXPIRES = process.env.JWT_ACCESS_EXPIRES || '1d';
+export const JWT_REFRESH_EXPIRES = process.env.JWT_REFRESH_EXPIRES || '30d';
+export const PORT = 8005;
